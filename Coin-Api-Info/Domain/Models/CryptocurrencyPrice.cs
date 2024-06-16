a09a5db5da0a7e62bffd2694cb16f2250c264ed9
Raegar04 <mykyta.bieloshapka@nure.ua>
2024-06-16 16:52:44 +0300

    Building dependencies structure. DbContext and repository implementation

---
+++ Coin-Api-Info/Domain/Models/CryptocurrencyPrice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models;
+
+public class CryptocurrencyPrice
+{
+    public Guid Id { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateTime LastUpdated { get; set; }
+
+    public Guid CryptocurrencyId { get; set; }
+
+    public virtual Cryptocurrency Cryptocurrency { get; set; }
+}
---
+++ Coin-Api-Info/Persistence/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.DataAccess;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Cryptocurrency> Cryptocurrencies { get; set; }
+    public DbSet<CryptocurrencyPrice> CryptocurrencyPrices { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cryptocurrency>().
+            HasOne(a => a.Price).
+            WithOne(a=>a.Cryptocurrency).
+            HasForeignKey("CryptocurrencyId").
+            OnDelete(DeleteBehavior.Cascade);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
